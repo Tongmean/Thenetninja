26bfa9293db06594b9a1ead547a60f538cf19a60
Tongmean Teang <tongmean.teang@wdc.com>
2022-10-18 12:56:31 +0700

    4

--- readingist-ninja/src/reducers/bookReducer.js
+++ readingist-ninja/src/reducers/bookReducer.js
@@ -2,12 +2,13 @@ export const bookReducer = (state, action) =>{
     switch(action.type){
         case 'ADD_BOOK':
             return[
-                ...state,
+                
                 {
                     title: action.book.title,
                     author: action.book.author,
-                    id: Math.random()
-                }
+                    id: Math.random()*10
+                },
+                ...state
             ];
         case 'REMOVE_BOOK':
             return state.filter(book=>book.id !== action.id);
